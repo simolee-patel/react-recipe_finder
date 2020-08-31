40f0ef9b971515ce656eb180f225b3ddc81bcc72
simolee <patelsimolee12@gmail.com>
2020-08-31 11:21:19 -1100

    Updates

---
+++ precache-manifest.10b3d4c0a1eb6c5b146854ae28807a72.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2975ab1e2a196040e3526c556b98f2d8",
+    "url": "/react-recipe_finder/index.html"
+  },
+  {
+    "revision": "87a425f92917125cf18b",
+    "url": "/react-recipe_finder/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "328ff4b2343ae805d7a8",
+    "url": "/react-recipe_finder/static/css/main.9ec31a67.chunk.css"
+  },
+  {
+    "revision": "87a425f92917125cf18b",
+    "url": "/react-recipe_finder/static/js/2.ece80cae.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react-recipe_finder/static/js/2.ece80cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328ff4b2343ae805d7a8",
+    "url": "/react-recipe_finder/static/js/main.674f7f4f.chunk.js"
+  },
+  {
+    "revision": "73dc24715209e623a443",
+    "url": "/react-recipe_finder/static/js/runtime-main.c404498d.js"
+  },
+  {
+    "revision": "3400cdc1ba776722b0952941a4ef3032",
+    "url": "/react-recipe_finder/static/media/aboutus.3400cdc1.png"
+  },
+  {
+    "revision": "fb7121df4a10610435e7ad7467099abe",
+    "url": "/react-recipe_finder/static/media/home.fb7121df.png"
+  }
+]);
